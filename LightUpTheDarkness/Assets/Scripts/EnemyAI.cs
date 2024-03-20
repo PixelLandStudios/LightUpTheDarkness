f6f7aee36d438b76c66bc1d92ad3f4d929c59e79
PixelLand Studios <mohammad.alown@gmail.com>
2024-03-20 07:50:41 +0300

    Added Attack Animation for the Monster

--- LightUpTheDarkness/Assets/Scripts/EnemyAI.cs
+++ LightUpTheDarkness/Assets/Scripts/EnemyAI.cs
@@ -3,21 +3,28 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public enum EnemyState { Idle, Follow, Retreat }
+    public enum EnemyState { Walk, Follow, Retreat, Attack, Idle }
     public float followDistance = 10f; // Adjust this distance as needed
     public float retreatDistance = 5f; // Distance at which the enemy starts retreating
     public float retreatSpeed = 2f; // Speed at which the enemy retreats
-    public float idleRadius = 5f; // Radius within which the enemy wanders in idle state
+    public float walkingRadius = 5f; // Radius within which the enemy wanders in idle state
+    public float attackDuration = 1.5f; // Duration of attack animation
+    public float idleDuration = 5.5f; // Duration of attack animation
 
-    private EnemyState currentState = EnemyState.Idle;
+    private EnemyState currentState = EnemyState.Walk;
     private Transform playerTransform;
     private NavMeshAgent navMeshAgent;
     private Vector3 initialPosition;
     private Vector3 idleDestination;
+    private bool isAttacking = false;
+    private float attackTimer = 0f;
+    private float idleTimer = 0f;
 
-    [SerializeField] Material IdleMat;
-    [SerializeField] Material FollowMat;
-    [SerializeField] MeshRenderer FaceMeshRenderer;
+    [SerializeField] Animator EnemyAnimator;
+    [SerializeField] RuntimeAnimatorController IdleAnimation;
+    [SerializeField] RuntimeAnimatorController WalkAnimation;
+    [SerializeField] RuntimeAnimatorController FollowAnimation;
+    [SerializeField] RuntimeAnimatorController AttackAnimation;
 
     // Reference to the flashlight GameObject
     [SerializeField] GameObject flashlight;
@@ -36,17 +43,16 @@ public class EnemyAI : MonoBehaviour
 
         switch (currentState)
         {
-            case EnemyState.Idle:
-                FaceMeshRenderer.material = IdleMat;
+            case EnemyState.Walk:
+                EnemyAnimator.runtimeAnimatorController = WalkAnimation;
+                navMeshAgent.speed = 0.5f;
 
-                // Check if the player is within the follow distance
                 if (distanceToPlayer <= followDistance)
                 {
                     currentState = EnemyState.Follow;
                     break;
                 }
 
-                // Check if the enemy reached its idle destination
                 if (Vector3.Distance(transform.position, idleDestination) < navMeshAgent.stoppingDistance)
                 {
                     SetRandomIdleDestination();
@@ -54,21 +60,24 @@ public class EnemyAI : MonoBehaviour
                 break;
 
             case EnemyState.Follow:
-                FaceMeshRenderer.material = FollowMat;
+                EnemyAnimator.runtimeAnimatorController = FollowAnimation;
+                navMeshAgent.speed = 1f;
 
-                // Check if the player is within the follow distance
-                if (distanceToPlayer > followDistance)
+                if (distanceToPlayer <= navMeshAgent.stoppingDistance) //check if attack is on cooldown
                 {
-                    currentState = EnemyState.Idle;
+                    currentState = EnemyState.Attack;
+                    AttackPlayer();
+                }
+                else if (distanceToPlayer > followDistance)
+                {
+                    currentState = EnemyState.Walk;
                     SetRandomIdleDestination();
                 }
                 else
                 {
-                    // Check if the flashlight is pointing towards the enemy and it's turned on
                     Vector3 directionToEnemy = (transform.position - flashlight.transform.position).normalized;
                     if (Vector3.Dot(flashlight.transform.forward, directionToEnemy) > 0.5f && flashlight.GetComponent<Light>().enabled)
                     {
-                        // Calculate direction away from player
                         Vector3 retreatDirection = (transform.position - playerTransform.position).normalized;
                         Vector3 retreatPosition = transform.position + retreatDirection * retreatDistance;
 
@@ -78,34 +87,67 @@ public class EnemyAI : MonoBehaviour
                     }
                     else
                     {
-                        // Calculate path to the player and set it as the destination
                         navMeshAgent.SetDestination(playerTransform.position);
                     }
                 }
                 break;
 
             case EnemyState.Retreat:
-                // Check if the player is within the follow distance
                 if (distanceToPlayer > followDistance)
                 {
-                    currentState = EnemyState.Idle;
+                    currentState = EnemyState.Walk;
+                    SetRandomIdleDestination();
+                }
+                break;
+
+            case EnemyState.Attack:
+                if (!isAttacking)
+                {
+                    AttackPlayer();
+                }
+                else
+                {
+                    attackTimer += Time.deltaTime;
+                    if (attackTimer >= attackDuration)
+                    {
+                        currentState = EnemyState.Idle;
+                        StayIdle();
+                        isAttacking = false;
+                        attackTimer = 0f;
+                    }
+                }
+                break;
+
+            case EnemyState.Idle:
+                idleTimer += Time.deltaTime;
+                if (idleTimer >= idleDuration)
+                {
+                    idleTimer = 0f;
+                    currentState = EnemyState.Walk;
                     SetRandomIdleDestination();
                 }
                 break;
         }
     }
 
-    // Set a random destination within idleRadius
+    void AttackPlayer()
+    {
+        isAttacking = true;
+        EnemyAnimator.runtimeAnimatorController = AttackAnimation;
+    }
+
+    void StayIdle()
+    {
+        EnemyAnimator.runtimeAnimatorController = IdleAnimation;
+    }
+
     void SetRandomIdleDestination()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * idleRadius;
+        Vector3 randomDirection = Random.insideUnitSphere * walkingRadius;
         randomDirection += initialPosition;
         NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, idleRadius, 1);
+        NavMesh.SamplePosition(randomDirection, out hit, walkingRadius, 1);
         idleDestination = hit.position;
         navMeshAgent.SetDestination(idleDestination);
-
-        //if I need the enemy to stay in it's area I need to comment this line
-        //initialPosition = idleDestination; // Update initialPosition
     }
 }
